a7a8dc43996df62157a3705d5d9fd9874f8b6722
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-12-12 23:12:01 +0100

    oijsajnd

--- js/script.js
+++ js/script.js
@@ -1,43 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
     const logoSection = document.querySelector('.logo');
     const containerNavi = document.querySelector('.container-navi');
-    
+
     setTimeout(function () {
-      const arrow = document.createElement('span');
-      arrow.innerHTML = '<i class="fas fa-chevron-down"></i>';
-      arrow.classList.add('arrow');
-  
-      const navContainer = document.createElement('div');
-      navContainer.appendChild(arrow);
-      navContainer.appendChild(containerNavi);
-  
-      logoSection.appendChild(navContainer);
-  
-      arrow.addEventListener('click', function () {
-        containerNavi.style.display = 'block';
-  
-        containerNavi.innerHTML = '';
-  
-        const aboutLink = createNavLink('about.html', 'About');
-        const projectsLink = createNavLink('projects.html', 'Projects');
-        const contactLink = createNavLink('contact.html', 'Contact');
-  
-        containerNavi.appendChild(aboutLink);
-        containerNavi.appendChild(projectsLink);
-        containerNavi.appendChild(contactLink);
-  
-      
-        arrow.style.display = 'none';
-      });
-    }, 1000); 
-  
+        const arrow = document.createElement('span');
+        arrow.innerHTML = '<i class="fas fa-chevron-down"></i>';
+        arrow.classList.add('arrow');
+
+        const navContainer = document.createElement('div');
+        navContainer.appendChild(arrow);
+        navContainer.appendChild(containerNavi);
+
+        logoSection.appendChild(navContainer);
+
+        arrow.addEventListener('click', function () {
+            containerNavi.style.display = 'block';
+           
+            containerNavi.classList.add('fade-in');
+
+            containerNavi.innerHTML = '';
+
+            const aboutLink = createNavLink('about.html', 'About');
+            const projectsLink = createNavLink('projects.html', 'Projects');
+            const contactLink = createNavLink('contact.html', 'Contact');
+
+            containerNavi.appendChild(aboutLink);
+            containerNavi.appendChild(projectsLink);
+            containerNavi.appendChild(contactLink);
+
+            arrow.style.display = 'none';
+
+            setTimeout(function () {
+                containerNavi.classList.add('show');
+            }, 1000);
+        });
+    }, 1000);
+
     function createNavLink(href, text) {
-      const link = document.createElement('a');
-      const linkText = document.createElement('h2');
-      link.href = href;
-      link.classList.add('navi');
-      linkText.textContent = text;
-      link.appendChild(linkText);
-      return link;
+        const link = document.createElement('a');
+        const linkText = document.createElement('h2');
+        link.href = href;
+        link.classList.add('navi');
+        linkText.textContent = text;
+        link.appendChild(linkText);
+        return link;
     }
-  });
+});
