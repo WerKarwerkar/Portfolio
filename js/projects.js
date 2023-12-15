2f38a949ffa8d4d7edd0b66b46cf8f1b424b0a9e
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-12-15 19:27:42 +0100

    fffff

--- js/projects.js
+++ js/projects.js
@@ -6,16 +6,12 @@ let sectionIndex = 0;
 const totalSections = initialSlider.children.length;
 
 leftArrow.addEventListener('click', function () {
-    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : totalSections - 1;
-    updateSliderTransform();
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    initialSlider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
 });
 
 rightArrow.addEventListener('click', function () {
-    sectionIndex = (sectionIndex < totalSections - 1) ? sectionIndex + 1 : 0;
-    updateSliderTransform();
+    sectionIndex = (sectionIndex < 3 ) ? sectionIndex + 1 : 3;
+    initialSlider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
 });
 
-function updateSliderTransform() {
-    const transformPercentage = (sectionIndex / totalSections) * -100;
-    initialSlider.style.transform = 'translateX(' + transformPercentage + '%)';
-}
