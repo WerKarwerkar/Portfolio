7e7825c5bbdcfba410111c0f846fc4ae023c5cc7
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-12-17 18:08:11 +0100

    kjshdkjdh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const logoSection = document.querySelector('.logo');
     const containerNavi = document.querySelector('.container-navi');
-    let emailDisplayed = false; 
+    let emailDisplayed = false;
 
     setTimeout(function () {
         const arrow = document.createElement('span');
@@ -16,14 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
         arrow.addEventListener('click', function () {
             containerNavi.style.display = 'block';
-
             containerNavi.classList.add('fade-in');
-
             containerNavi.innerHTML = '';
 
             const aboutLink = createNavLink('about.html', 'About');
             const projectsLink = createNavLink('projects.html', 'Projects');
-            const contactLink = createNavLink('contact.html', 'Contact', showEmail);
+            const contactLink = createContactLink('Contact', showEmail);
 
             containerNavi.appendChild(aboutLink);
             containerNavi.appendChild(projectsLink);
@@ -37,10 +35,19 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }, 1000);
 
-    function createNavLink(href, text, clickHandler) {
+    function createNavLink(href, text) {
         const link = document.createElement('a');
         const linkText = document.createElement('h2');
-        link.href = 'javascript:void(0)'; 
+        link.href = href;
+        link.classList.add('navi');
+        linkText.textContent = text;
+        link.appendChild(linkText);
+        return link;
+    }
+
+    function createContactLink(text, clickHandler) {
+        const link = document.createElement('div');
+        const linkText = document.createElement('h2');
         link.classList.add('navi');
         linkText.textContent = text;
         link.appendChild(linkText);
@@ -53,13 +60,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function showEmail() {
-        if (!emailDisplayed) { 
+        if (!emailDisplayed) {
             const emailContainer = document.createElement('h3');
-            emailContainer.classList.add('email'); 
+            emailContainer.classList.add('email');
             emailContainer.textContent = 'Email: werkar51641@stud.noroff.no';
-        
             containerNavi.appendChild(emailContainer);
-            emailDisplayed = true; 
+            emailDisplayed = true;
         }
     }
 });
